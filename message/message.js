a56d6bba72c9a6a631cf75ea9821be47b6d92845
KeishiroNagaseforWin10 <keit0728.job@gmail.com>
2020-12-20 02:25:17 +0900

    ファイル書き込み用メッセージ追加

--- message/message.js
+++ message/message.js
@@ -12,7 +12,9 @@ module.exports = {
     NO_SPLITTABLE_FILES_FOUND:  '分割可能なファイルが存在しません。',
     NOT_FOUND_SETTINGFILES:     'settings.iniファイルが存在しません。',
     OUT_OF_TARGET_FILES:        '分割できるファイルの拡張子は.csv、.txtです。',
-    SUCCESS_READ_FILE:          'ファイルの読み込みに成功しました。',
+    READ_FILE_SUCCESS:          'ファイルの読み込みに成功しました。',
+    WRITE_FILE_SUCCESS:         'ファイルの書き込みに成功しました。',
+    WRITE_FILE_FAILURE:         'ファイルの書き込みに失敗しました。',
     START_SPILIT_FILE:          'ファイルの分割を開始しました。',
     FINISH_SPILIT_FILE:         'ファイルの分割が完了しました。',
 }
